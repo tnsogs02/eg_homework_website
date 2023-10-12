59ce724128e846a29adf103cb9db41618f16dda2
HaretaSK <andy19991214@gmail.com>
2023-10-12 16:40:19 +0800

    use parameterized statement to insert messages

--- sendMsg.php
+++ sendMsg.php
@@ -1,5 +1,8 @@
 <?php
     require_once('dbconn.php');
-    $sql = sprintf("INSERT INTO messages (author, title, content) VALUES ('%s', '%s', '%s')", $_SERVER['REMOTE_ADDR'], $_POST['title'], $_POST['content']);
-    $dbconn->query($sql);
+
+    $query_insertMsg = $dbconn->prepare("INSERT INTO messages (author, title, content) VALUES (?, ?, ?)");
+    $query_insertMsg->bind_param("sss", $_SERVER['REMOTE_ADDR'], $_POST['title'], $_POST['content']);
+    $query_insertMsg->execute();
+    $query_insertMsg->close();
 ?>
