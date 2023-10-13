5cd6dd1c3b04b653fc7c7a996ce7f5f49f841ccd
HaretaSK <andy19991214@gmail.com>
2023-10-13 10:23:16 +0800

    use js to concat/escape messages instead of php

--- message.php
+++ message.php
@@ -1,22 +1,42 @@
-<p><h1>留言板作業</h1></p>
-<p id="messageBoard"></p>
+<div><h1>留言板作業</h1></div>
+<div id="messageBoard"></div>
 
-<p><form id="sendMsgForm" method="POST">
+<div><form id="sendMsgForm" method="POST">
 <input type="text" name="title" value="標題" maxlength="100" style="font-weight: bold;"/><br>
 <input type="text" name="content" value="內容" maxlength="2048"/><br>
 <input type="submit" id="sendMsgButton" value="送出留言"/>
-</form></p>
+</form></div>
 
 <script>
+
+  function escapeChar(input) {
+    let div = document.createElement('div');
+    div.innerText = input;
+    return div.innerHTML;
+  }
 
   function getMsg () {
     let req = new XMLHttpRequest(); 
     req.open('GET', '/getMsg.php', true);
+    req.responseType = 'json';
     req.send();
     req.onload = function () {
-      document.querySelector('#messageBoard').innerHTML = this.responseText;
-    };
-  };
+      if (this.status == 200) {
+        let msgDisplay = "";
+        let resp = this.response
+        if (resp.length > 0) {
+          for(let i = 0; i < resp.length; i++) {
+            msgDisplay += '<hr>';
+            msgDisplay += `<h3>#${escapeChar(resp[i].id)} ${escapeChar(resp[i].title)}</h3>${escapeChar(resp[i].content)}<br>from: ${escapeChar(resp[i].author)}`;
+          }
+        } else {
+          msgDisplay += '<h3>留言板上什麼也沒有</h3>';
+        }
+        msgDisplay += '<hr>';
+        document.querySelector('#messageBoard').innerHTML = msgDisplay;
+      }
+    }
+  }
   
   getMsg();
 
